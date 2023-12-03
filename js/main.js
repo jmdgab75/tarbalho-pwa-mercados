233b77f3a60d57de9976cbdbcfe92c7fac53212b
jmdgab75 <gabrieljmd75@gmail.com>
2023-12-03 15:54:48 -0400

    commit banco

--- js/main.js
+++ js/main.js
@@ -1,52 +1,86 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', async () => {
-    try {
-      let reg;
-      reg = await navigator.serviceWorker.register('/sw.js', { type: "module" });
-      console.log('Service worker registrada!', reg);
-    } catch (err) {
-      console.log('Service worker registro falhou', err);
-    }
-  });
-}
+// Inicialização do IndexedDB
+const dbName = "supermercadosDB";
+const dbVersion = 1;
+const request = indexedDB.open(dbName, dbVersion);
 
-let posicaoInicial;
-const capturarLocalizacao = document.getElementById( 'localizacao');
-const latitude = document.getElementById( 'latitude' );
-const longitude = document.getElementById( 'longitude' );
- 
-let gmap_canvas = document.getElementById ("gmap_canvas");
-
-const sucesso = (posicao) => {
-    posicaoInicial = posicao;
-    latitude.innerHTML = posicaoInicial.coords.latitude;
-    longitude.innerHTML = posicaoInicial.coords.longitude;
-    map.src = "https://maps.google.com/maps?q" + 
-
-
-    const gmap_canvas.src = "https://maps.google.com/maps?q=636+5th+Ave%2C+New+York&t=&z=13&ie=UTF8&iwloc=&output=embed";
-    
-}
-
-const erro = (error) => {
-    let errorMessage;
-    switch(error.code){
-    case 0:
-         errorMessage = "Erro desconhecido"
-    break;
-    case 1:
-          errorMessage = "Permissão negada!"
-    break;
-    case 2:
-          errorMessage = "Captura de posição indisponível!"
-    break;
-    case 3:
-          errorMessage = "Tempo de excedido!"
-    break;
-  }
-    console.log( 'Ocorreu um erro:' + errorMessage);
+request.onerror = (event) => {
+  console.error("Erro ao abrir o banco de dados:", event.target.error);
 };
 
-capturarLocalizacao.addEventListener('click',() => {
-    navigator.geolocation.getCurrentPosition(sucesso, erro);
+request.onupgradeneeded = (event) => {
+  const db = event.target.result;
+  const objectStore = db.createObjectStore("supermercados", { keyPath: "id", autoIncrement: true });
+  objectStore.createIndex("nome", "nome", { unique: false });
+  objectStore.createIndex("localizacao", "localizacao", { unique: false });
+};
+
+// Função para cadastrar supermercado
+const cadastrarSupermercado = () => {
+  const nome = document.getElementById("nome").value;
+  const localizacao = document.getElementById("localizacao").value;
+
+  const supermercado = { nome, localizacao };
+
+  const transaction = request.result.transaction(["supermercados"], "readwrite");
+  const objectStore = transaction.objectStore("supermercados");
+  const addRequest = objectStore.add(supermercado);
+
+  addRequest.onsuccess = () => {
+    console.log("Supermercado cadastrado com sucesso!");
+    listarSupermercados();
+  };
+
+  addRequest.onerror = (event) => {
+    console.error("Erro ao cadastrar supermercado:", event.target.error);
+  };
+};
+
+// Função para listar supermercados
+const listarSupermercados = () => {
+  const transaction = request.result.transaction(["supermercados"], "readonly");
+  const objectStore = transaction.objectStore("supermercados");
+  const cursorRequest = objectStore.openCursor();
+
+  const listContainer = document.getElementById("supermarketList");
+  listContainer.innerHTML = "";
+
+  cursorRequest.onsuccess = (event) => {
+    const cursor = event.target.result;
+
+    if (cursor) {
+      const li = document.createElement("li");
+      li.textContent = `${cursor.value.nome} - ${cursor.value.localizacao}`;
+      listContainer.appendChild(li);
+
+      cursor.continue();
+    }
+  };
+
+  cursorRequest.onerror = (event) => {
+    console.error("Erro ao listar supermercados:", event.target.error);
+  };
+};
+
+// Inicialização do mapa
+const initializeMap = () => {
+  const mapContainer = document.getElementById("map");
+  const localizacao = document.getElementById("localizacao").value;
+
+  const mapUrl = `https://www.google.com/maps/embed/v1/view?key=AIzaSyDsdNjeP_Uroc-PGBFZBigxSEa3uPsU1QQ&center=${localizacao}&zoom=15`;
+
+  mapContainer.innerHTML = `<iframe
+    width="100%"
+    height="300"
+    frameborder="0" style="border:0"
+    src="${mapUrl}"
+    allowfullscreen
+  ></iframe>`;
+};
+
+document.getElementById("supermarketForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+  cadastrarSupermercado();
 });
+
+listarSupermercados();
+initializeMap();
